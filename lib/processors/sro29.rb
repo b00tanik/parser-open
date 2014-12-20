aeb9ec27b6ab052c1bdc8f7f8a0a2cc70afd6e60
m1chael <asoi@mail.ru>
2014-12-20 15:25:06 +0000

    Sro29#list_links added

--- lib/processors/sro29.rb
+++ lib/processors/sro29.rb
@@ -1,4 +1,22 @@
+# encoding: UTF-8
 class Sro29
+  
+  FIELDS = [
+      :inn,
+      :name,
+      :short_name,
+      :city,
+      :status,
+      :resolution_date,
+      :legal_address,
+      :certificate_number,
+      :ogrn
+    ]
+  
+  def initialize
+    @data, @links = [], ['http://www.sro29.ru/index.php/reestr-sro']
+  end
+  
   def perform
     list_links
     iterate
@@ -7,7 +25,13 @@ class Sro29
   private
   
   def list_links
-  
+    list_link = @links[0]
+    
+    while list_link.length > 0
+      doc = Nokogiri::HTML(open(list_link))
+      list_link = doc.xpath('//a[text()="следующая страница"]/@href').first.to_s
+      @links << list_link if list_link.length > 0
+    end
   end
 
   def iterate
--- spec/sro29_spec.rb
+++ spec/sro29_spec.rb
@@ -1,6 +1,15 @@
 require 'processors/sro29'
+require 'spec_helper'
 
 describe Sro29 do
+  
+  let(:links) { [
+    'http://www.sro29.ru/index.php/reestr-sro', 
+    'http://www.sro29.ru/index.php/component/content/article?id=567',
+    'http://www.sro29.ru/index.php/component/content/article?id=568',
+    'http://www.sro29.ru/index.php/component/content/article?id=569'
+    ] }  
+  
   before (:each) do
     @test = Sro29.new
   end
@@ -20,7 +29,20 @@ describe Sro29 do
       expect(@test).to receive(:iterate)
       @test.perform
     end
+
+    # it 'should return array with 334 elements' do
+    #   data = @test.perform
+    #   expect(data.class).to eq(Array)
+    #   expect(data.length).to eq(334)
+    # end
   
   end
+  
+  describe '#list_links' do
+    it 'should collect links to links variable' do
+      @test.perform
+      expect(@test.instance_variable_get(:@links)).to eq(links)
+    end
+  end  
   
 end
