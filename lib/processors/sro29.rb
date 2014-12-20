60ea03efd6aa0c371d0d82b8211b231374b8c625
m1chael <asoi@mail.ru>
2014-12-20 14:36:19 +0000

    list_links and iterate functions added

--- lib/processors/sro29.rb
+++ lib/processors/sro29.rb
@@ -1,5 +1,16 @@
 class Sro29
   def perform
+    list_links
+    iterate
+  end
   
+  private
+  
+  def list_links
+  
+  end
+
+  def iterate
+    
   end
 end
--- spec/sro29_spec.rb
+++ spec/sro29_spec.rb
@@ -9,4 +9,18 @@ describe Sro29 do
     expect(@test).to respond_to(:perform)
   end
   
+  describe '#perform' do
+    
+    it 'should call #list_links' do
+      expect(@test).to receive(:list_links)
+      @test.perform
+    end
+    
+    it 'should call #iterate' do
+      expect(@test).to receive(:iterate)
+      @test.perform
+    end
+  
+  end
+  
 end
